fe5fcc2c15ba2b0c16ca65b7518b2dbfb09b1861
Leshik1305 <realleshik1305@gmail.com>
2025-11-18 21:33:13 +0300

    add filters in get_products

--- app/models/products.py
+++ app/models/products.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 
-from sqlalchemy import String, Boolean, Float, Integer, ForeignKey, Numeric
+from sqlalchemy import String, Boolean, Float, Index, Integer, ForeignKey, Numeric, Computed
+from sqlalchemy.dialects.postgresql import TSVECTOR
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
@@ -12,7 +13,7 @@ class Product(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     description: Mapped[str | None] = mapped_column(String(500), nullable=True)
-    price: Mapped[float] = mapped_column(Float, nullable=False)
+    price: Mapped[Decimal] = mapped_column(Float, nullable=False)
     image_url: Mapped[str | None] = mapped_column(String(200), nullable=True)
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
@@ -20,8 +21,19 @@ class Product(Base):
         ForeignKey("categories.id"), nullable=False
     )
     seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
-    rating: Mapped[float] = mapped_column(Float, default=0.0)
+    tsv: Mapped[TSVECTOR] = mapped_column(TSVECTOR, Computed("""
+            setweight(to_tsvector('english', coalesce(name, '')), 'A')
+            || 
+            setweight(to_tsvector('english', coalesce(description, '')), 'B')
+            """,persisted=True,), nullable=False,)
+    rating: Mapped[float] = mapped_column(Float, default=0.0, server_default='0')
     # rating: Mapped[float] = mapped_column(Numeric(10, 2), default=0.0)
     category: Mapped["Category"] = relationship("Category", back_populates="products")
     seller: Mapped["User"] = relationship("User", back_populates="products")
     reviews: Mapped[list["Review"]] = relationship("Review", back_populates="product")
+    cart_items: Mapped[list["CartItem"]] = relationship("CartItem", back_populates="product",
+                                                        cascade="all, delete-orphan")
+
+    __table_args__ = (
+        Index("ix_products_tsv_gin", "tsv", postgresql_using="gin"),
+    )
